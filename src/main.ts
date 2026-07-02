a15e4701798668f1d41d2c1baded7e40e3d0078f
copc <102230084@sv1.dut.udn.vn>
2026-07-03 02:24:21 +0700

    fixed: setup port to let connect

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
